74394f5bcefe4d74352173ba8365a9efd7e30c49
test <test@gmail.com>
2016-06-03 10:42:58 -0700

    support boost.context v1. on boost1.59

--- libgo/ctx_boost_context/context.h
+++ libgo/ctx_boost_context/context.h
@@ -2,7 +2,7 @@
 
 #if BOOST_EXECUTION_CONTEXT == 2  // boost version >= 1.61
 # include "context_v2.h"
-#elif BOOST_EXECUTION_CONTEXT == 1  // boost version <= 1.60
+#else  // boost version <= 1.60
 # include "context_v1.h"
 #endif
 
