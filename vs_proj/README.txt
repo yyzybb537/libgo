0c0cc488a2062b9a40c6ea4f0a61b8881cef9e3f
yyz <289633152@qq.com>
2016-06-05 12:49:13 +0800

    vs projects make tutorial

--- vs_proj/README.txt
+++ vs_proj/README.txt
@@ -5,3 +5,7 @@
 3.启动git bash, 并切换到你正在阅读的这个README文件所在目录
 4.执行脚本  ./make_vs_projs.sh
 5.在当前目录下会生成不同版本的vs工程文件, 直接使用即可
+
+另外, 如果你想运行测试代码和教程代码, 需要安装boost,
+并且在执行脚本时给出boost的安装目录, 例如:
+  $ ./make_vs_projs.sh -DBOOST_ROOT="E:\\boost_1_61_0"
--- vs_proj/make_vs_projs.sh
+++ vs_proj/make_vs_projs.sh
@@ -4,20 +4,20 @@ mkdir vs2013/x86 vs2013/x64 vs2015/x86 vs2015/x64 -p
 
 rm vs2013/x86/* -rf
 cd vs2013/x86
-cmake ../../.. -G"Visual Studio 12 2013"
+cmake ../../.. -G"Visual Studio 12 2013" $@
 cd ../..
 
 rm vs2013/x64/* -rf
 cd vs2013/x64
-cmake ../../.. -G"Visual Studio 12 2013 Win64"
+cmake ../../.. -G"Visual Studio 12 2013 Win64" $@
 cd ../..
 
 rm vs2015/x86/* -rf
 cd vs2015/x86
-cmake ../../.. -G"Visual Studio 14 2015"
+cmake ../../.. -G"Visual Studio 14 2015" $@
 cd ../..
 
 rm vs2015/x64/* -rf
 cd vs2015/x64
-cmake ../../.. -G"Visual Studio 14 2015 Win64"
+cmake ../../.. -G"Visual Studio 14 2015 Win64" $@
 cd ../..
