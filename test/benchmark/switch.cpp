c07baf5770674bd16a89ca9a0f3d5089139546d2
yuyangzi <yuyangzi@meizu.com>
2016-10-27 03:40:03 -0700

    切换速度性能测试代码

---
+++ test/benchmark/switch.cpp
@@ -0,0 +1,59 @@
+#include <boost/thread.hpp>
+#include <chrono>
+#include <iostream>
+#include <atomic>
+#include <libgo/libgo.h>
+#include <cmath>
+
+#define COC 4
+static uint64_t counter[COC] = {};
+static uint64_t last_c = 0;
+static int coro = 5;
+
+void foo(int i)
+{
+    for (;;) {
+        ++counter[i];
+        co_yield;
+    }
+}
+
+void show()
+{
+    uint64_t c = 0;
+    for (int i = 0; i < COC; ++i)
+        c += counter[i];
+    uint64_t sec_c = c - last_c;
+    last_c = c;
+    std::cout << "Switch " << coro * coro << " times: " << sec_c << std::endl;
+}
+
+int main()
+{
+    
+    boost::thread_group tg;
+    for (int i = 0; i < COC; ++i)
+        tg.create_thread([]{ co_sched.RunLoop(); });
+
+    co_sched.GetOptions().enable_work_steal = false;
+    tg.create_thread([]{
+                for (;;) {
+                    sleep(1);
+                    show();
+                }
+            });
+
+    for (int i = 0; i < coro*coro; ++i)
+        go [=]{ foo(i % COC); };
+
+    for (;;) {
+        for (int i = coro*coro; i < (coro+1)*(coro+1); ++i)
+            go [=]{ foo(i % COC); };
+        coro++;
+        sleep(3);
+    }
+
+    return 0;
+}
+
+
