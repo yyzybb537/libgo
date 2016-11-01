200f0c71c13d90ef10c1eafa9bb477f976ac398b
yuyangzi <yuyangzi@meizu.com>
2016-10-31 19:42:05 -0700

    repair golang test

--- test/golang/test.sh
+++ test/golang/test.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 echo "------------- libgo ---------------"
-g++ libgo_test.cpp -std=c++11 -O3 -llibgo -o libgo_test && ./libgo_test
+g++ libgo_test.cpp -std=c++11 -O3 -o libgo_test -llibgo -lboost_context -lboost_thread -pthread -static && ./libgo_test
 echo "-----------------------------------"
 
 echo "------------- golang --------------"
-go test -test.bench=".*"
+go test golang_test.go -test.bench=".*"
 echo "-----------------------------------"
