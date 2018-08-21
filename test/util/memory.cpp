0c45e0ab952f65fb3bdea802ea7b7faa61e48f65
test <test@gmail.com>
2018-08-21 21:51:06 +0800

    删除一批废弃的测试代码, 增加测试内存消耗的代码

---
+++ test/util/memory.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <iomanip>
+#include <chrono>
+#include <thread>
+#include <map>
+#include "../../libgo/libgo.h"
+using namespace std;
+using namespace std::chrono;
+
+#define OUT(x) cout << #x << " = " << x << endl
+#define O(x) cout << x << endl
+
+struct Timer { Timer() : tp(system_clock::now()) {} virtual ~Timer() { auto dur = system_clock::now() - tp; O("Cost " << duration_cast<milliseconds>(dur).count() << " ms"); } system_clock::time_point tp; };
+struct Bench : public Timer { Bench() : val(0) {} virtual ~Bench() { stop(); } void stop() { auto dur = system_clock::now() - tp; O("Per op: " << duration_cast<nanoseconds>(dur).count() / std::max(val, 1L) << " ns"); auto perf = (double)val / duration_cast<milliseconds>(dur).count() / 10; if (perf < 1) O("Performance: " << std::setprecision(3) << perf << " w/s"); else O("Performance: " << perf << " w/s"); } Bench& operator++() { ++val; return *this; } Bench& operator++(int) { ++val; return *this; } Bench& add(long v) { val += v; } long val; };
+
+int main() {
+    for (int i = 0; i < 1000000; ++i)
+        go []{};
+
+    for (;;)
+        sleep(10);
+}
---
+++ test/util/run_memory.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+g++ -std=c++11 memory.cpp -L../../build -llibgo && ./a.out
