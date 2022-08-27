53649ad9993a1f3cf9e6129380f8d5b1d26abf92
test <test@gmail.com>
2022-08-27 11:11:03 +0800

    cmake构建过程兼容mac

--- third_party/select_asm.sh
+++ third_party/select_asm.sh
@@ -10,5 +10,6 @@ obj_name=`basename $obj`
 source=`echo $obj_name | cut -d\. -f1`.S
 file=$work/third_party/boost.context/libs/context/src/asm/$source
 cp $file $work/libgo/context
-sed -i "s/${2}_fcontext/libgo_${2}_fcontext/g" $work/libgo/context/$source
+sed "s/${2}_fcontext/libgo_${2}_fcontext/g" $work/libgo/context/$source > tmp_$source
+mv tmp_$source $work/libgo/context/$source
 echo -n $work/libgo/context/$source
